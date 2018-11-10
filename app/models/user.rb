f334bb14a601c910c34630bbf3bb33e151ccb14c
allan_horst <allanhorstwillig@gmail.com>
2018-11-10 18:10:29 -0200

    criado scaffold goals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_many :goals
   validates :name, :email, :salary, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_195113) do
+ActiveRecord::Schema.define(version: 2018_11_10_195710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.string "description"
+    t.float "value"
+    t.date "due_on"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
